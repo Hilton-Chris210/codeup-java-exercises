6143995da65e8abd875d60c8e7639be2ead68b79
Chris Hilton <hilton.chris210@gmail.com>
2023-02-09 11:37:51 -0600

    morning push

---
+++ src/HelloWorld.java
@@ -0,0 +1,60 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        System.out.print("Hello World!, ");
+//        System.out.print("Hello World!");
+
+
+//        int myFavoriteNumber = 55;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "3.1456";
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+////        x = x / y;
+//        x /= y;
+////        y = y - x;
+//        y -= x;
+
+        byte x = 127;
+        x += x;
+
+        system
+
+
+        System.out.println(x);
+//        System.out.println(y);
+
+
+
+
+
+
+    }
+}
