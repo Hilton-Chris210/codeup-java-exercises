5cc634e431a6035e067e7ba35814f51d47a9b824
Chris Hilton <hilton.chris210@gmail.com>
2023-02-10 10:31:22 -0600

    morning push

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,34 +3,35 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 
-//double pi = 3.14159;
-//System.out.format("The value of pi is approximately %.2f.%n", pi);
+//      double pi = 3.14159;
 //
-//        Scanner scanner = new Scanner(System.in);
+//      String message = String.valueOf(System.out.format("The value of pi is approximately %.2f.%n", pi));
+//      System.out.println(message);
 //
-//        System.out.print("Enter an integer: ");
-//        int userInput = scanner.nextInt();
+//          Scanner scanner = new Scanner(System.in);
 //
-//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//          System.out.print("Enter an integer: ");
+//          int userInput = scanner.nextInt();
 //
+//          System.out.println("You entered: --> \"" + userInput + "\" <--");
+////
 
-
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Please enter the number of strings you want to enter: ");
-////takes an integer input
-//        String[] string = new String [sc.nextInt()];
-////consuming the <enter> from input above
-//        sc.nextLine();
-//        for (int i = 0; i < string.length; i++)
-//        {
-//            string[i] = sc.nextLine();
-//        }
-//        System.out.println("\nYou have entered: ");
-////for-each loop to print the string
-//        for(String str: string)
-//        {
-//            System.out.println(str);
-//        }
+//              Scanner sc = new Scanner(System.in);
+//              System.out.print("Please enter the number of strings you want to enter: ");
+                //takes an integer input
+//              String[] string = new String [sc.nextInt()];
+//              //consuming the <enter> from input above
+//              sc.nextLine();
+        //      for (int i = 0; i < string.length; i++)
+        //        {
+        //            string[i] = sc.nextLine();
+        //        }
+        //        System.out.println("\nYou have entered: ");
+        ////for-each loop to print the string
+        //        for(String str: string)
+        //        {
+        //            System.out.println(str);
+        //        }
 
 
 //        Scanner myObj = new Scanner(System.in);
@@ -51,16 +52,14 @@ public class ConsoleExercises {
 
         Scanner myObj = new Scanner(System.in);
 
-
-
         System.out.println("Enter Length and width of your room:");
 
         // String input
         float length = myObj.nextFloat();
         float width = myObj.nextFloat();
 
-        System.out.println("The perimeter of your room is: " + ((length * 2) + (width * 2)) + ", " + "The area of your room is: " + (length * width / 2));
-//        System.out.println("The area of your room is: " + (length * width / 2));
+        System.out.println("The perimeter of your room is: " + ((length + width)*2) + ", " + "The area of your room is: " + (length * width / 2));
+        System.out.println("The area of your room is: " + (length * width / 2));
 
 
 
